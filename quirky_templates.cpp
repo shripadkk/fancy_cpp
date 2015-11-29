004592a32a36e227de8a0bf79ea6a432c9b7b4cb
shripad <shripad.k.kulkarni@gmail.com>
2015-11-29 23:01:16 +0530

    Added an example to show what happens when templates are used

--- quirky_templates.cpp
+++ quirky_templates.cpp
@@ -1,1 +1,24 @@
-kasdkjshsdak
+#include <iostream>
+
+template <typename T>
+void func(const T& val)
+{
+    static int count = 0;
+    std::cout <<"Entered val: "<<val<<" value of count is "<<count++<<std::endl;
+}
+
+int main()
+{
+    /* Pretty interesting stuff with static and object instantiation*/
+    double val1 = 1.1;
+    double val2 = 1.15;
+    int val3 =  1;
+
+    /* Keep an eye on the value of count
+     * one will notice when the value of
+     * count gets incremented
+     */
+    func(val3);
+    func(val2);
+    func(val1);
+}
